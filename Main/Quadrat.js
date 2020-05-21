73b040c997d76b53ee3bcc4141c23651e5025c70
Dani Vilardell <danivilardell@gmail.com>
2020-05-21 21:57:50 +0200

    hola que tal

---
+++ Main/Quadrat.js
@@ -0,0 +1,30 @@
+package Main;
+
+class quadrat {
+
+	constructor(x, y) {
+		this._x = x;
+		this._y = y;
+	}
+
+	dibuix() {
+		colorRect(this._x, this._y, MIDAQUADRAT, MIDAQUADRAT, 'white');
+	}
+
+	set x (x) {
+		this._x = x;
+	}
+
+	set y (y) {
+		this._y = y;
+	}
+
+	get x() {
+		return this._x;
+	}
+
+	get y() {
+		return this._y;
+	}
+
+}
---
+++ Quadrat.js
@@ -0,0 +1,29 @@
+
+class Quadrat {
+
+	constructor(x, y) {
+		this._x = x;
+		this._y = y;
+	}
+
+	dibuix() {
+		colorRect(this._x, this._y, MIDAQUADRAT, MIDAQUADRAT, 'white');
+	}
+
+	set x (x) {
+		this._x = x;
+	}
+
+	set y (y) {
+		this._y = y;
+	}
+
+	get x() {
+		return this._x;
+	}
+
+	get y() {
+		return this._y;
+	}
+
+}
