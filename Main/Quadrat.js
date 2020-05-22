beefcd9fe8564d40d63577f8257380ada5a86253
Dani Vilardell <danivilardell@gmail.com>
2020-05-22 14:11:28 +0200

    hola que tal

--- Main/Quadrat.js
+++ Main/Quadrat.js
@@ -20,4 +20,53 @@ class Quadrat {
 		return this._p;
 	}
 
+	estaTocantDE(mapa, DELTAPOS, MIDAQUADRAT) {
+		if (this._p.y - DELTAPOS < mapa.zonaJoc[0].y
+			|| this._p.x - DELTAPOS < mapa.zonaJoc[0].x) return true;
+		for (var i = 0; i < mapa.zonaNoJoc.length - 1; i += 2) {
+			if (this._p.x - DELTAPOS < mapa.zonaNoJoc[i].x + mapa.zonaNoJoc[i + 1].x
+				&& this._p.y - DELTAPOS < mapa.zonaNoJoc[i].y + mapa.zonaNoJoc[i + 1].y
+				&& this._p.x + MIDAQUADRAT> mapa.zonaNoJoc[i].x
+				&& this._p.y + MIDAQUADRAT > mapa.zonaNoJoc[i].y) return true;
+		}
+		return false;
+	}
+
+	estaTocantDD(mapa, DELTAPOS, MIDAQUADRAT) {
+		if(this._p.x + MIDAQUADRAT + DELTAPOS > mapa.zonaJoc[0].x + mapa.zonaJoc[1].x
+			|| this._p.y - DELTAPOS < mapa.zonaJoc[0].y) return true;
+		for (var i = 0; i < mapa.zonaNoJoc.length - 1; i += 2) {
+			if (this._p.x + DELTAPOS + MIDAQUADRAT > mapa.zonaNoJoc[i].x
+				&& this._p.y - DELTAPOS < mapa.zonaNoJoc[i].y + mapa.zonaNoJoc[i + 1].y
+				&& this._p.y + MIDAQUADRAT > mapa.zonaNoJoc[i].y
+				&& this._p.x < mapa.zonaNoJoc[i].x + mapa.zonaNoJoc[i + 1].x) return true;
+		}
+		return false;
+	}
+
+	estaTocantBE(mapa, DELTAPOS, MIDAQUADRAT) {
+		if (this._p.x - DELTAPOS < mapa.zonaJoc[0].x
+			|| this._p.y + MIDAQUADRAT + DELTAPOS > mapa.zonaJoc[0].y + mapa.zonaJoc[1].y) return true;
+		for (var i = 0; i < mapa.zonaNoJoc.length - 1; i += 2) {
+			if (this._p.x - DELTAPOS < mapa.zonaNoJoc[i].x + mapa.zonaNoJoc[i + 1].x
+				&& this._p.y + DELTAPOS + MIDAQUADRAT > mapa.zonaNoJoc[i].y
+				&& this._p.x + MIDAQUADRAT > mapa.zonaNoJoc[i].x
+				&& this._p.y < mapa.zonaNoJoc[i].y + mapa.zonaNoJoc[i + 1].y
+				) return true;
+		}
+		return false;
+	}
+
+	estaTocantBD(mapa, DELTAPOS, MIDAQUADRAT) {
+		if (this._p.x + MIDAQUADRAT + DELTAPOS > mapa.zonaJoc[0].x + mapa.zonaJoc[1].x
+			|| this._p.y + MIDAQUADRAT + DELTAPOS > mapa.zonaJoc[0].y + mapa.zonaJoc[1].y) return true;
+		for (var i = 0; i < mapa.zonaNoJoc.length - 1; i += 2) {
+			if (this._p.x + DELTAPOS + MIDAQUADRAT > mapa.zonaNoJoc[i].x
+				&& this._p.y + DELTAPOS + MIDAQUADRAT > mapa.zonaNoJoc[i].y
+				&& this._p.x < mapa.zonaNoJoc[i].x + mapa.zonaNoJoc[i + 1].x
+				&& this._p.y < mapa.zonaNoJoc[i].y + mapa.zonaNoJoc[i + 1].y) return true;
+		}
+		return false;
+	}
+
 }
